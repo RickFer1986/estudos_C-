18f74468975cf30deabf423b3e6c2eabc6f655e1
RickFer1986 <ricardo.lazafer@gmail.com>
2022-03-07 22:47:20 -0300

    Commit - Interpolação, Placeholder e Concatenação

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+string nome = "Rick";
+int idade = 35;
+char genero = 'M';
+double salario = 1.800;
+
+// ### Placeholder ###
+System.Console.WriteLine("{0} do sexo {1} com {2} anos já ganha um salário de R$ {3:F4}", nome, genero, idade, salario);
+
+// ### Interpolação ###
+System.Console.WriteLine($"{nome} do sexo {genero} com {idade} anos já ganha um salário de R$ {salario:F4}");
+
+// ### Concatenação ###
+System.Console.WriteLine(nome + " do sexo " + genero + " com " + idade + " anos já ganha um salário de R$ " + salario.ToString("F4"));
+
