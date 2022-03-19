371e3b7a7f2e089e19cbf2d761ee56e355735e21
RickFer1986 <ricardo.lazafer@gmail.com>
2022-03-19 18:34:07 -0300

    Commit - Introduçao a entrada de dados C#

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Globalization;
 
-string nome = "Rick";
-int idade = 35;
+
+// ### Exercicio ###
+string produto1 = "Computador";
+string produto2 = "Mesa de escritorio";
+
+byte idade = 30;
+int codigo = 5290;
 char genero = 'M';
-double salario = 1.800;
 
-// ### Placeholder ###
-System.Console.WriteLine("{0} do sexo {1} com {2} anos já ganha um salário de R$ {3:F4}", nome, genero, idade, salario);
+double preco1 = 2100.0;
+double preco2 = 650.50;
+double medida = 53.234567;
 
-// ### Interpolação ###
-System.Console.WriteLine($"{nome} do sexo {genero} com {idade} anos já ganha um salário de R$ {salario:F4}");
-
-// ### Concatenação ###
-System.Console.WriteLine(nome + " do sexo " + genero + " com " + idade + " anos já ganha um salário de R$ " + salario.ToString("F4"));
-
+System.Console.WriteLine("Produtos:");
+System.Console.WriteLine(produto1 + ", cujo preço é " + preco1);
+System.Console.WriteLine(produto2 + ", cujo preço é " + preco2);
+System.Console.WriteLine();
+System.Console.WriteLine("Registro: " + idade + " anos de idade, código " + codigo + " e gênero " + genero);
+System.Console.WriteLine();
+System.Console.WriteLine("Medida com oito casa decimais: " + medida.ToString("F8"));
+System.Console.WriteLine("Arredondando (três casas decimais): " + medida.ToString("F3"));
+System.Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
