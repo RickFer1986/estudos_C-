5fdafd6537401621c4e05d5ca2b3224dbae516f1
RickFer1986 <ricardo.lazafer@gmail.com>
2022-03-21 22:30:22 -0300

    Commit - Entrada de dados e casting

--- Program.cs
+++ Program.cs
@@ -24,3 +24,45 @@ System.Console.WriteLine();
 System.Console.WriteLine("Medida com oito casa decimais: " + medida.ToString("F8"));
 System.Console.WriteLine("Arredondando (três casas decimais): " + medida.ToString("F3"));
 System.Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+
+System.Console.WriteLine("================ USO DO CAST ======================");
+
+int x = 5;
+int y = 2;
+
+double result = (double) x / y;
+
+System.Console.WriteLine(result);
+
+System.Console.WriteLine("======================================");
+// Float contem 4 Bytes
+// Double contém 8 Bytes
+
+double a = 5.1;
+float b = (float) a;
+
+System.Console.WriteLine(b);
+
+System.Console.WriteLine("=============== Entrada de dados =======================");
+
+System.Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine();
+System.Console.WriteLine("--------");
+
+System.Console.Write("Digite seu idade: ");
+string nasc = Console.ReadLine();
+System.Console.WriteLine("--------");
+
+System.Console.Write("Digite seu estado civil: ");
+string sexo = Console.ReadLine();
+System.Console.WriteLine("--------");
+
+System.Console.WriteLine("Nome: " + nome);
+System.Console.WriteLine("Idade: " + nasc);
+System.Console.WriteLine("Sexo: " + sexo);
+System.Console.WriteLine("--------");
+
+System.Console.Write("Deseja salvar seus dados? S/N? ");
+string salvar = Console.ReadLine();
+
+System.Console.WriteLine("Dados salvos com sucesso!");
